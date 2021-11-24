ea2d9e20ab47aa6e7d1b02e374722238518d4f48
ariel <201710012@est.umss.edu>
2021-11-24 10:38:10 -0400

    archivos .c agregados

---
+++ leerftp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLEN 1024
+
+
+void changeToRoot(){
+	if((setuid(0)) < 0) printf("\n<br>setuid: operacion no permitida\n");
+	if((setgid(0)) < 0) printf("\n<br>setgid: operacion no permitida\n");
+}
+
+int main(void)
+{
+    changeToRoot();
+    system("mkdir test");
+    printf ("Content-type:text/html\n\n");
+    printf("<TITLE>Response</TITLE>\n");
+
+    printf("<br>Datos Formulario: %s\n");
+    printf("<br>Tama&ntildeo: %d\n");
+
+    printf("<p> Mensaje: %s");
+    printf("<p> Usuario: %s");
+    printf("<p> Clave: %s");
+    printf("<p> Shell: %s");
+
+    return 0;
+} 
